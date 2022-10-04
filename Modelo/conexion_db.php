dca9f81101fb02245dd0df33c5926551f6267556
camila2411fina@gmail.com <camila2411fina@gmail.com>
2022-10-04 14:03:43 -0500

    se agrega la base datos en linea

--- Modelo/conexion_db.php
+++ Modelo/conexion_db.php
@@ -2,6 +2,6 @@
 session_start();
 
 // $conn = mysqli_connect('localhost', 'id19409844_frank_admin', 'D2~+6SrMBnnPS[ZE','id19409844_controlserviciosocial');
-$conn = mysqli_connect('localhost', 'root', 'frank2022','control_servicio_social');
+$conn = mysqli_connect('biqx0gcp2ohntesmiiug-mysql.services.clever-cloud.com', 'utx7hhwklfyhoxgc', 'Vg6HniPrzxeV3UqHfmDc','biqx0gcp2ohntesmiiug');
 
 ?>
